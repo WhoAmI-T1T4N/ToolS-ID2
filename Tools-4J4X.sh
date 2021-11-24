e5dd32cc47d430558bc13954695520bd5a96093b
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-24 12:11:32 +0700

    Update and rename Tools-Males.sh to Tools-4J4X.sh

---
+++ Tools-4J4X.sh
@@ -0,0 +1,102 @@
+# Woyy, Subrek Channel Gua
+# Author TITAN
+# Channel T1T4N TZY
+
+green='\033[0;32m'
+greenlight='\033[32;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+black='\033[30;1m'
+yellow='\033[33;1m'
+
+# registrasi dulu
+echo $white "Utamakan Untuk REGISTRASI Dan PASSnya ADA DI CHANNEL T1T4N TZY"
+      read -p "UsernameNya :" nm ;
+      read -p "PassWordnya Apa : " pass ;
+      sleep 0.1
+
+# passwordnya
+if [ $pass = "KejuJoget" ]
+   then
+   echo
+   echo $red ######################
+   echo $red ## PASSWORD SUCCESS ##
+   echo $red ######################
+   sleep 0.1
+   else
+   echo "COBA LAGIII"
+   echo "CEK YOUTUBE T1T4N TZY"
+   sleep 3
+   sh Tools-4J4X.sh
+   sleep 0.1
+fi
+# coddingan termux dan kali linux
+echo $yellow    "@@@@@@@@@@@@@@@@@@@"
+echo $yeloow "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo $yellow"@@@" $black"000" $yellow"@@@@@@@@@@"  $black"000" $yellow"@@@"
+echo $yellow"@@@" $black"000" $yellow"@@@@@@@@@@" $black"000" $yellow"@@@"
+echo $yellow"@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo $yellow"@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo $yellow"@"$black"========================"$yellow"@"
+echo $yellow"@"$black"========================"$yellow"@"
+echo $yellow"@@"$black"======================"$yellow"@@"
+echo $yellow"@@@"$black"===================="$yellow"@@@"
+echo $yellow"@@@@"$black"=================="$yellow"@@@@"
+echo $yellow"@@@@@"$black"================"$yellow"@@@@"
+echo $yellow"@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo
+echo
+echo $green        "=============================="
+echo $red          "Author : Mr.T1T4N"
+echo $red          "Team : Mugiwara BlackHat"
+echo $red          "Youtube : T1T4N TZY"
+echo $green        "=============================="
+echo $green        "==========================================="
+echo $red          "1]. Tools CCTV"
+echo $red          "2]. Tools DDOS ID"
+echo $red          "3]. Tools DIKIT ID"
+echo $red          "4]. Tools CCTV BANYAK"
+echo $red          "5]. Tools DDOS SPOON"
+echo $red          "00]. Keluar Karena Ada Tanda Bahayaa....."
+echo $green        "==========================================="
+read -p "Pilih Aja Tools Sesuka Mu Yahh :" pepek
+if [ $pepek = 1 ]
+then
+     wget https://raw.githubusercontent.com/MrVirusSpm-07/cchek/main/cchek.sh
+       sh cchek.sh
+fi
+if [ $pepek = 2 ]
+then
+    wget https://raw.githubusercontent.com/WhoAmI-T1T4N/DDoS-EPIC-ID/main/MuGiWARA-DDOS.py
+       python2 MuGiWARA-DDOS.py
+fi
+if [ $pepek = 3 ]
+then
+   wget https://raw.githubusercontent.com/WhoAmI-T1T4N/TooLs-D1K1T/main/Tools-Sultan.sh
+       sh Tools-Sultan.sh
+fi 
+if [ $pepek = 4 ]
+then
+   wget https://raw.githubusercontent.com/MrVirusSpm-07/cctv-pub/main/cctv.py
+      python2 cctv.py
+fi 
+if [ $pepek = 5 ]
+then
+   wget https://raw.githubusercontent.com/BLACK-PHANTOM-CYBER/Dos-stealh/master/mec.py
+     python2 mec.py
+fi
+if [ $pepek = 00 ]
+then
+echo "444444444444" "00000000000"       "6666666666666"
+echo "444444444444" "00000000000"    "6666666666666666666"
+echo "4444444"                       "6666"         "6666"   "777777777777"
+echo "4444444"      "555555555555"   "6666"         "6666"   "777777777777"
+echo "44444444444"  "555555555555"   "6666666666666666666"   "777777777777"
+echo "44444444444"  "555555555555"   "6666666666666666666"   "777777777777"
+echo     "4444444"  "555555555555"   "6666666666666666666"   "777777777777"
+echo     "4444444"  "555555555555"   "6666"          "6666"  "77777777777777777"
+echo     "444444"   "555555555555"  "6666"            "6666" "77777777777777777"
+echo "44444444444"  "555555555555"                           "77777777777777777"
+echo "4444444444"   "555555555555"
+echo "444444444"
+exit
